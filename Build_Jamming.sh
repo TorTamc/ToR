e38d54aef70dc9559c13217d4bad947dfc3cf0df
TorTamc <86399340+TorTamc@users.noreply.github.com>
2021-06-24 14:00:27 +0700

    Add files via upload

---
+++ Build_Jamming.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir /home/pi/Desktop/Jamming
+mkdir /home/pi/Desktop/Jamming/Output
+mkdir /home/pi/Desktop/Jamming/Output/PSD
+
+mkdir /home/pi/Desktop/Jamming/Output/PSD/Jam
+mkdir /home/pi/Desktop/Jamming/Output/PSD/Jam/L1
+mkdir /home/pi/Desktop/Jamming/Output/PSD/Jam/L5
+mkdir /home/pi/Desktop/Jamming/Output/PSD/Jam/Power_L1
+mkdir /home/pi/Desktop/Jamming/Output/PSD/Jam/Power_L5
+
+mkdir /home/pi/Desktop/Jamming/Output/PSD/NoJam
+mkdir /home/pi/Desktop/Jamming/Output/PSD/NoJam/L1
+mkdir /home/pi/Desktop/Jamming/Output/PSD/NoJam/L5
+mkdir /home/pi/Desktop/Jamming/Output/PSD/NoJam/Power_L1
+mkdir /home/pi/Desktop/Jamming/Output/PSD/NoJam/Power_L5
+
+mkdir /home/pi/Desktop/Jamming/Output/PSD/Status_Device
+
+cp /home/pi/ToR-main/PSD_loop.py /home/pi/Desktop/Jamming
+cp /home/pi/ToR-main/upload_Jam.py /home/pi/Desktop/Jamming
+cp /home/pi/ToR-main/upload_NoJam.py /home/pi/Desktop/Jamming
